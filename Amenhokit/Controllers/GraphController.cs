bbfb17c435d9dc271e71c9d0509dbbd962d2e8e9
Morten Albring <electrika.ma@gmail.com>
2015-04-13 15:11:12 +0100

    Added viewmodel and display in view

--- Amenhokit/Controllers/GraphController.cs
+++ Amenhokit/Controllers/GraphController.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Amenhokit.Models;
+using Amenhokit.Models.ViewModel;
 
 namespace Amenhokit.Controllers
 {
     public class GraphController : Controller
     {
+
+        private DataContext db = new DataContext();
         //
         // GET: /Graph/
 
         public ActionResult Index()
         {
-            return View();
+            var players = db.Player.ToList();
+
+            var viewmodel = new List<PlayerScore>();
+
+            foreach (var p in players)
+            {
+                var scores = db.PlayerGame.Where(e => e.PlayerID == p.ID);
+
+                foreach (var s in scores)
+                {
+                    var pscore = new PlayerScore {Player = p, GameID = s.GameID, TotalScore = s.TotalScore};
+
+                    viewmodel.Add(pscore);
+                }
+
+            }
+
+
+            return View(viewmodel);
         }
 
     }
---
+++ Amenhokit/Models/ViewModel/PlayerScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Amenhokit.Models.ViewModel
+{
+    public class PlayerScore
+    {
+        public DateTime Date { get; set; }
+        public Player Player { get; set; }
+        public int GameID { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
