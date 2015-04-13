c964c29ec1c68b995894eca5fe999912e39caf32
Morten Albring <electrika.ma@gmail.com>
2015-04-13 11:16:34 +0100

    Added Controller and views

---
+++ Amenhokit/Controllers/GameController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amenhokit.Models;
+
+namespace Amenhokit.Controllers
+{
+    public class GameController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View(db.Game.ToList());
+        }
+
+        //
+        // GET: /Game/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Game/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Game.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Game/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Game/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Game/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Game.Find(id);
+            db.Game.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
