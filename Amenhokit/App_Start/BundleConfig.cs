194cd9bb15e613733515d8a9039a592194cc96be
Morten Albring <electrika.ma@gmail.com>
2015-04-13 14:32:19 +0100

    Updated jquery and added jquery migrate

--- Amenhokit/App_Start/BundleConfig.cs
+++ Amenhokit/App_Start/BundleConfig.cs
@@ -8,8 +8,9 @@ namespace Amenhokit
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(                
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-migrate-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
