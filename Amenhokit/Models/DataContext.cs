6b00dbf81aa1f26627333f157a8086f37adcca7b
Morten Albring <electrika.ma@gmail.com>
2015-04-13 11:06:44 +0100

    Added new models and context

---
+++ Amenhokit/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Amenhokit.Models
+{
+    public class DataContext : DbContext
+    {
+              public DataContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Player> Player { get; set; }
+        public DbSet<Game> Game { get; set; }
+    }
+}
---
+++ Amenhokit/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Amenhokit.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public int GameID { get; set; }
+        public int PlayerID { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
